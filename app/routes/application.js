5a16df9d1266e68720b3cf1dfdb79d5ddaa0539c
Felix Rieseberg <felix@felixrieseberg.com>
2016-02-24 10:59:58 -0800

    :books: Add Window Menu

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
+import setupMenu from '../utils/window-menu';
 
 const {Route} = Ember;
 
 export default Route.extend({
+    beforeModel() {
+        setupMenu();
+    },
+
     model() {
         return this.store.findAll('blog');
     }
