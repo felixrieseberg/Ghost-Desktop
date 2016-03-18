a93fbc0989e3dfe8b9ad05f3180a91975969e3ce
Felix Rieseberg <felix@felixrieseberg.com>
2016-03-18 13:30:01 -0700

    :memo: Enable Ghost Instance Context Menu
    - Enables the context menu in Ghost instances

--- main/preload.js
+++ main/preload.js
@@ -4,6 +4,31 @@
  * This file is being preloaded in Ghost Instances.
  * ⚠ Remember: No jQuery! ⚠
  */
+var remote = require('electron').remote;
+var Menu   = remote.Menu;
+
+var template = [{
+    label: 'Undo',
+    role: 'undo'
+}, {
+    label: 'Redo',
+    role: 'redo'
+}, {
+    type: 'separator'
+}, {
+    label: 'Cut',
+    role: 'cut'
+}, {
+    label: 'Copy',
+    role: 'copy'
+}, {
+    label: 'Paste',
+    role: 'paste'
+}, {
+    label: 'Select All',
+    role: 'selectall'
+}];
+var editorMenu = Menu.buildFromTemplate(template);
 
 /**
  * Simple timeout function checking for 
@@ -13,8 +38,8 @@
 function checkStatus() {
     var err = document.querySelector('p.main-error');
     var loaded = document.querySelector('a[title="New Post"]');
-    
-    
+
+
     if (err && ((err.childElementCount && err.childElementCount > 0) || (err.textContent && err.textContent.length > 0))) {
         // Noooo, login errors!
         console.log(`login-error`)
@@ -26,4 +51,38 @@ function checkStatus() {
     }
 }
 
+/**
+ * Setup the context menu
+ */
+function handleContextMenu(e) {
+    // Do nothing when there's no input nearby
+    if (!e.target.closest('textarea, input, [contenteditable="true"]')) {
+        return;
+    };
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    var node = e.target;
+
+    while (node) {
+        if (node.nodeName.match(/^(input|textarea)$/i) || node.isContentEditable) {
+            editorMenu.popup(remote.getCurrentWindow());
+            break;
+        }
+
+        node = node.parentNode;
+    }
+}
+
+/**
+ * Init
+ */
 setTimeout(checkStatus, 100);
+window.addEventListener('contextmenu', handleContextMenu);
+
+/**
+ * Cleanup
+ */
+remote = undefined;
+Menu = undefined;
