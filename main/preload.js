3d0ec85e7915c57ec2c84c81c2b153d6613982ca
Bhargav Nookala <nooknb@gmail.com>
2016-07-11 16:57:40 -0700

    :wrench: Adding a better upgrade notification

--- main/preload.js
+++ main/preload.js
@@ -1,6 +1,7 @@
 // Preload various modules before actual loading
 // the individual Ghost blogs
 require('./preload/check-login');
+require('./preload/upgrade-notification');
 require('./preload/dragdrop');
 require('./preload/spellchecker');
 require('./preload/external-links');
