372bb71b46679c1bc1ddcfac464ca4388fbfbefe
Felix Rieseberg <felix@felixrieseberg.com>
2016-08-30 21:47:31 +0200

    :wrench: Update contributors only if file >24h old

--- scripts/fetch-contributors.js
+++ scripts/fetch-contributors.js
@@ -1,6 +1,28 @@
 "use strict";
 
 const fetch = require('node-fetch');
+const fs = require('fs');
+const util = require('util');
+const contributorsFilePath = 'public/contributors.json';
+
+fs.stat(contributorsFilePath, (err, stats) => {
+    if (err && err.code === 'ENOENT') {
+        // File does not exist, move to fetch right away
+        return fetchAndWriteContributorsFile();
+    } else if (err) {
+        throw err;
+    };
+
+
+    const mtime = new Date(util.inspect(stats.mtime));
+    const maxAge = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
+
+    if (mtime < maxAge) {
+        // File exists, but is too old
+        console.log('Contributors file on disk, but older than 24 hours.')
+        fetchAndWriteContributorsFile();
+    }
+});
 
 /**
  * Helpers
@@ -95,4 +117,23 @@ function fetchContributors() {
     });
 }
 
-module.exports = fetchContributors;
+/**
+ * Fetch the contributors and write the result to disk
+ */
+function fetchAndWriteContributorsFile() {
+    fs.access(contributorsFilePath, fs.F_OK | fs.R_OK | fs.W_OK, (error) => {
+        if (!error) {
+            console.log('Deleting existing contributors file');
+            fs.unlinkSync(contributorsFilePath);
+        }
+
+        console.log('Fetching contributors');
+
+        fetchContributors()
+            .then((data) =>  {
+                console.log('Building new contributor file\n');
+                fs.writeFileSync(contributorsFilePath, JSON.stringify(data));
+                console.log('Done');
+            });
+    });
+}
