556892dfc53b5dbdd6bb6bd062bc703822d0f0d5
Hannah Wolfe <github.erisds@gmail.com>
2016-09-02 21:02:34 +0200

    🎨 Use randomcolor for blog icon colours (#196)
    - switches color-scheme for randomcolor
    - only generate one colour at a time

--- app/utils/color-picker.js
+++ app/utils/color-picker.js
@@ -1,4 +1,5 @@
-const ColorScheme = require('color-scheme');
+const randomColor = require('randomcolor');
+const colorSettings = {luminosity: 'light'};
 
 /**
  * Generates a random set of (nicely selected!) colors and selects one to be returned.
@@ -7,30 +8,13 @@ const ColorScheme = require('color-scheme');
  * @return {string}
  */
 export default function getIconColor(excludedColor) {
-    let scheme = new ColorScheme();
-
-    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-    scheme.from_hue(50)
-        .scheme('triade')
-        .variation('soft')
-        .web_safe(true);
-    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
-
-    let colors = scheme.colors();
+    let color = randomColor(colorSettings);
 
     // Make sure our generated colors don't contain the color
     // that's already set.
-    if (excludedColor !== null) {
-        // We store colors on the model with the '#' prefixed.
-        let colorName = excludedColor.substr(1);
-        let excludedColorIndex = colors.indexOf(colorName);
-
-        if (excludedColorIndex >= 0) {
-            colors.removeAt(excludedColorIndex);
-        }
+    while (excludedColor === color) {
+        color = randomColor(colorSettings);
     }
 
-    let index = Math.floor(Math.random() * colors.length);
-
-    return ['#', colors[index]].join('');
+    return color;
 };
