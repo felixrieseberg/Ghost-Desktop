a2e091d84140cdf3d8bc3f9e4f2963514cce351d
Felix Rieseberg <felix@felixrieseberg.com>
2016-09-03 12:43:37 +0200

    :wrench: Ensure Prototype Extensions

--- config/environment.js
+++ config/environment.js
@@ -12,7 +12,10 @@ module.exports = function (environment) {
                 // e.g. 'with-controller': true
             },
             EXTEND_PROTOTYPES: {
-                Date: false
+                Date: false,
+                Array: true,
+                String: true,
+                Function: true
             }
         },
 
