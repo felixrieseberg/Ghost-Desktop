9bb27f752c3c84bfda7c1c1a03f40195975f5eba
Felix Rieseberg <felix@felixrieseberg.com>
2016-10-27 09:59:41 -0700

    :package: Update build scripts

--- scripts/create-osx-build.js
+++ scripts/create-osx-build.js
@@ -4,40 +4,81 @@ const lazyRequire = require('lazy-require');
 const path = require('path');
 const fs = require('fs');
 const appdmg = lazyRequire('appdmg');
+const sign = require('electron-osx-sign');
 
-if (appdmg instanceof Error) {
-    console.log('Building Ghost Desktop for OS X requires appdmg.');
-    console.log('Automatic installation was attempted, but failed.');
-    console.log('\nYou can probably fix this error by running "npm install appdmg"\n');
-    console.log(appdmg.stack);
+/**
+ * Signs the application package in a way that hopefully
+ * satisfies Apple.
+ */
+function signAppBundle() {
+    const app = path.join(__dirname, '..', 'electron-builds/Ghost-darwin-x64/Ghost.app');
+    const nm = path.join(app, 'Contents/Resources/app/node_modules');
+    const platform = 'darwin';
+    const identity = '6RKE5ET24A';
+    const binaries = [
+        path.join(nm, 'keytar/Build/Release/keytar.node'),
+        path.join(nm, 'spellchecker/Build/Release/hunspell.a'),
+        path.join(nm, 'spellchecker/Build/Release/spellchecker.node')
+    ];
 
-    return;
+    return new Promise((resolve, reject) => {
+        sign({
+            app,
+            binaries,
+            identity,
+            platform
+        }, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
 }
 
-// Create appdmg
-const source = path.join('.appdmg.json');
-const dir = path.join('electron-builds', 'Ghost-darwin-x64-dmg');
-const target = path.join(dir, 'ghost.dmg');
+function createDmg() {
+    return new Promise((resolve, reject) => {
+        // Create appdmg
+        const source = path.join('.appdmg.json');
+        const dir = path.join('electron-builds', 'Ghost-darwin-x64-dmg');
+        const target = path.join(dir, 'ghost.dmg');
 
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        const ee = appdmg({ source, target });
+
+        ee.on('progress', (info) => {
+            if (info.type === 'step-begin') {
+                console.log(`Starting ${info.title}`);
+            } else if (info.type === 'step-end') {
+                console.log(`Finished with status "${info.status}"`);
+            }
+        });
+
+        ee.on('finish', () => {
+            console.log('Ghost Desktop sucessfully packaged - find the dmg in ./electron-builds/!');
+            resolve();
+        });
+
+        ee.on('error', (err) => {
+            console.log('Building the Ghost Desktop DMG failed:')
+            console.log(err);
+            reject();
+        });
+    });
 }
 
-const ee = appdmg({ source, target });
+function main() {
+    if (appdmg instanceof Error) {
+        console.log('Building Ghost Desktop for OS X requires appdmg.');
+        console.log('Automatic installation was attempted, but failed.');
+        console.log('\nYou can probably fix this error by running "npm install appdmg"\n');
+        console.log(appdmg.stack);
 
-ee.on('progress', (info) => {
-    if (info.type === 'step-begin') {
-        console.log(`Starting ${info.title}`);
-    } else if (info.type === 'step-end') {
-        console.log(`Finished with status "${info.status}"`);
+        return;
     }
-});
 
-ee.on('finish', () => {
-    console.log('Ghost Desktop sucessfully packaged - find the dmg in ./electron-builds/!');
-});
+    signAppBundle().then(() => createDmg());
+}
 
-ee.on('error', (err) => {
-    console.log('Building the Ghost Desktop DMG failed:')
-    console.log(err);
-});
+main();
